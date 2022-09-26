7dc3f0974e2b8d644af4f8be2a21d620d049d5ca
ilikedonuts0120 <82606970+ilikedonuts0120@users.noreply.github.com>
2022-09-25 20:42:33 -0400

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,5 @@
 function addUser (){
-    user_name = document.getElementById("username").value;
-    localStorage.setItem("username", user_name);
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
     window.location = "kwitter_room.html";
 }
